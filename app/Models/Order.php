5f9b393a745a5d3cd2ac78f42198ac0b10598d9e
Seyit Mustafa Adem Kandemir <seyitmustafaademk@gmail.com>
2024-06-22 17:39:33 +0300

    created migration, model for orders table

---
+++ app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Order extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'orders';
+    protected $fillable = [
+        'user_id',
+        'uuid',
+        'amount',
+    ];
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
