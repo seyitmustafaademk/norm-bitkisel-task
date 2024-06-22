87fd9c827219fd4f846c2877d5488e81c421b85f
Seyit Mustafa Adem Kandemir <seyitmustafaademk@gmail.com>
2024-06-22 17:59:22 +0300

    created migration, model for order_products pivot table

---
+++ app/Models/OrderProduct.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class OrderProduct extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'order_products';
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'quantity',
+        'price',
+        'discount',
+    ];
+
+    public function order(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
