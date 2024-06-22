07964f272d394afe23c5bc9d884dc98c092e7b87
Seyit Mustafa Adem Kandemir <seyitmustafaademk@gmail.com>
2024-06-22 13:54:14 +0300

    created User Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'firstname' => 'Super',
+                'lastname' => 'Admin',
+                'birthday' => '1990-01-01',
+                'username' => 'superadmin',
+                'email' => 'super.admin@mail.com',
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+            ],
+            [
+                'firstname' => 'John',
+                'lastname' => 'Doe',
+                'birthday' => '1990-01-01',
+                'username' => 'johndoe',
+                'email' => 'john.doe@mail.com',
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+            ]
+        ];
+
+        if (User::count() > 0) {
+            return;
+        }
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
