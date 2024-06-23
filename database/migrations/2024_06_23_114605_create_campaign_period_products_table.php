bd7269131447fb2ee9e3fe7c5a3cff7f509815c1
Seyit Mustafa Adem Kandemir <seyitmustafaademk@gmail.com>
2024-06-23 15:03:19 +0300

    created migration, model for campaign_period_products table

---
+++ database/migrations/2024_06_23_114605_create_campaign_period_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('campaign_period_products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('campaign_id')->constrained();
+            $table->foreignId('period_id')->nullable()->constrained();
+            $table->foreignId('product_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('campaign_period_products');
+    }
+};
