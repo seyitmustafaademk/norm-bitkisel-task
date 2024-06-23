5b8d4925b963de69341903557d1be21ecaa985af
Seyit Mustafa Adem Kandemir <seyitmustafaademk@gmail.com>
2024-06-24 00:08:13 +0300

    created StoreRequest.php and UpdateRequest.php custom request validation for store and update api

---
+++ app/Http/Requests/Admin/Category/StoreRequest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Requests\Admin\Category;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:100',
+            'description' => 'required|string|max:65535',
+            'slug' => 'required|string|max:100|unique:categories,slug',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'The name field is required.',
+            'name.string' => 'The name field must be a string.',
+            'name.max' => 'The name field must not exceed 100 characters.',
+
+            'description.required' => 'The description field is required.',
+            'description.string' => 'The description field must be a string.',
+            'description.max' => 'The description field must not exceed 65535 characters.',
+
+            'slug.required' => 'The slug field is required.',
+            'slug.string' => 'The slug field must be a string.',
+            'slug.max' => 'The slug field must not exceed 100 characters.',
+            'slug.unique' => 'The slug field must be unique.',
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $response = response()->json([
+            'message' => 'The given data was invalid.',
+            'errors' => $validator->errors(),
+        ], 422);
+
+        throw new \Illuminate\Validation\ValidationException($validator, $response);
+    }
+
+}
