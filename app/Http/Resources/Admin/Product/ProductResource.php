9d16416efd87f601672751662bcb73a09302295e
Seyit Mustafa Adem Kandemir <seyitmustafaademk@gmail.com>
2024-06-24 17:55:09 +0300

    added id column in ProductResource.php

--- app/Http/Resources/Admin/Product/ProductResource.php
+++ app/Http/Resources/Admin/Product/ProductResource.php
@@ -16,6 +16,7 @@ class ProductResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
             'stock' => $this->stock,
