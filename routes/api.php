e38796e14b7f9254521608d0cdfea606d261668c
Seyit Mustafa Adem Kandemir <seyitmustafaademk@gmail.com>
2024-06-24 14:20:36 +0300

    product apis defined

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\Category\CategoryApiController;
 use App\Http\Controllers\Admin\Period\PeriodApiController;
+use App\Http\Controllers\Admin\Product\ProductApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -42,6 +43,15 @@ Route::prefix('/v1')->group(function () {
             Route::delete('/{id}', [PeriodApiController::class, 'destroy']);
             Route::delete('/{id}/force-delete', [PeriodApiController::class, 'forceDelete']);
             Route::patch('/{id}/restore', [PeriodApiController::class, 'restore']);
+        });
+        Route::prefix('/products')->group(function () {
+            Route::get('/', [ProductApiController::class, 'index']);
+            Route::get('/{id}', [ProductApiController::class, 'show']);
+            Route::post('/', [ProductApiController::class, 'store']);
+            Route::put('/{id}', [ProductApiController::class, 'update']);
+            Route::delete('/{id}', [ProductApiController::class, 'destroy']);
+            Route::delete('/{id}/force-delete', [ProductApiController::class, 'forceDelete']);
+            Route::patch('/{id}/restore', [ProductApiController::class, 'restore']);
         });
     });
 });
