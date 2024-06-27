3120602ef9ec84ca460f1c880fbc29b3d00d6dde
Seyit Mustafa Adem Kandemir <seyitmustafaademk@gmail.com>
2024-06-27 19:31:16 +0300

    import front-end assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-import './bootstrap';
+import axios from "axios";
+
+let token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['X-CSRF-TOKEN'] = token;
+
+// Get basket information
+axios.get('/api/user')
+    .then(function (response) {
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.error(error);
+    });
