d231e54618237d00f86cab9ac0b576b8878b2241
Seyit Mustafa Adem Kandemir <seyitmustafaademk@gmail.com>
2024-06-28 21:27:38 +0300

    added redeemed_at column in user_campaigns table

--- database/migrations/2024_06_23_120527_create_user_campaigns_table.php
+++ database/migrations/2024_06_23_120527_create_user_campaigns_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->foreignId('campaign_id')->constrained();
+            $table->timestamp('redeemed_at');
             $table->timestamps();
         });
     }
